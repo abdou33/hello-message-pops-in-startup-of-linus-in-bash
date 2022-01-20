fb99b5a51eb738bcad1a43869f6cf8d2184ed424
abdellah bouchama <abdo069874@gmail.com>
2022-01-20 20:11:59 +0100

    update

---
+++ startup.sh
@@ -0,0 +1,10 @@
+resize -s 6 1
+#echo $'\a';
+echo $'\n';
+echo "      H  H  EEEE  L     L     OOOO";
+echo "      H  H  E     L     L     O  O";
+echo "      HHHH  EEEE  L     L     O  O     "abdou""; #change "abdou to whatever you want"
+echo "      H  H  E     L     L     O  O";
+echo "      H  H  EEEE  LLLL  LLLL  OOOO";	sleep 2;
+#echo ($PPID);
+kill -1 $PPID;
